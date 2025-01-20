3e23016316a49bffee93810e0f997858ae703a02
이승환 <seaung13@gmail.com>
2025-01-20 21:43:32 +0900

    feat: MyBatis 동적 쿼리, 기타 기능 확인용 주석 ItemMapper.xml에 추가

--- src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
+++ src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -5,6 +5,7 @@ import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +17,7 @@ public interface ItemMapper {
 
     void update(@Param("id") Long id, @Param("updateParam")ItemUpdateDto updateParam);
 
+    @Select("select id, item_name, price, quantity from item where id = #{id}")
     Optional<Item> findById(Long id);
 
     List<Item> findAll(ItemSearchCond itemSearch);
